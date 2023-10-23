c126f8f6acd507687487d8c2ed62d4e880ba4461
eduardocDev <eduardoc.dev@gmail.com>
2023-10-23 17:15:49 -0300

    Aplicação de dicas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,20 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
+    <title>Dicas Tecnólogicas</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css">
 </head>
 <body>
     <header class="header" style="background-image: url('{{ asset('resources/views/assets/servers.jpg')}}')">
-        <h1>EduardocDev</h1>
+        <h1>Eduardo Carmo Dicas</h1>
         <nav class="nav">
             <button onclick="location.href='https://eduardocdev.000webhostapp.com/'">About Me</button>
         </nav>
     </header>
 
     <main class="main">
-        <h2>Welcome to my blog!</h2>
-        <p>Be Happy! this is a part of my blog, today i will show you how to use this blog</p>
+        <h2>Dica</h2>
+        <p>Descrição</p>
+        <a href="">Youtube.Link</a> - <a href="">Pinterest.Link</a>
     </main>
 
     <footer class="Contact">
