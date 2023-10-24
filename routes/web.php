0f25150a4dc5fa51b39097d7694a6deae955a8e6
eduardocDev <eduardoc.dev@gmail.com>
2023-10-24 11:24:12 -0300

    Alteração Content

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,3 @@ use App\Http\Controllers\PostsController;
 */
 
 Route::get('/', [PagesController::class, 'index']);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
